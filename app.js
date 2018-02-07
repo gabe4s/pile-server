2f31da2c8ee35fa553e8b14572578ad4523b627e
Gabe Douda <gabedouda@gmail.com>
2018-02-06 21:26:53 -0700

    added new templating engine files, and furthered template

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var path = require("path");
-var fs = require("fs");
+
+var fileUtils = require("./lib/file-utils.js");
+var templateEngine = require("./lib/template-engine.js");
 
 var PORT = 8080;
 
@@ -17,7 +19,7 @@ app.use(bodyParser.urlencoded({
 
 app.get('/*', function(req, res) {
     var directoryName = BASE_DIR + req.url;
-    readDirectory(directoryName).then(
+    fileUtils.readDirectory(directoryName).then(
         function(directory) {
             console.log(directory);
             res.status(200);
@@ -31,65 +33,6 @@ app.get('/*', function(req, res) {
         }
     )
 });
-
-function readDirectory(directoryName) {
-    return new Promise(
-        function(resolve, reject) {
-            fs.readdir(directoryName, function(err, items) {
-                if(!err && items) {
-                    var directory = {};
-                    directory.files = [];
-                    directory.dirs = [];
-        
-                    items.forEach(function(itemName) {
-                        var fullPathName = getFullPath(directoryName, itemName);
-                        if(isFile(fullPathName)) {
-                            directory.files.push(fullPathName);
-                        }
-                        if(isDirectory(fullPathName)) {
-                            directory.dirs.push(fullPathName);
-                        }
-                    });
-
-                    resolve(directory);
-                } else if (err) {
-                    reject(err);
-                } else {
-                    reject("Could not get files in path: " + directoryName);
-                }
-            });
-        }
-    );
-}
-
-function getFullPath(directoryName, itemName) {
-    var fullPath = directoryName;
-    if(directoryName[directoryName.length-1] != "/") {
-        fullPath += "/";
-    }
-    fullPath += itemName;
-    return fullPath;
-}
-
-function isFile(itemName) {
-    var isFile = false;
-
-    if(fs.lstatSync(itemName).isFile()) {
-        isFile = true;
-    }
-
-    return isFile;
-}
-
-function isDirectory(itemName) {
-    var isDirectory = false;
-
-    if(fs.lstatSync(itemName).isDirectory()) {
-        isDirectory = true;
-    }
-
-    return isDirectory;
-}
 
 console.log("Server listening on port " + PORT);
 app.listen(PORT);
