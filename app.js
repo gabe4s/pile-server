0c898e7428d3b383a81d7e0100cb5bb727bfdca4
Gabe Douda <gabedouda@gmail.com>
2018-02-07 17:35:45 -0700

    added express-handlebars as templating engine

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var express = require("express");
+var expressHandlebars = require("express-handlebars");
 var bodyParser = require('body-parser');
 var path = require("path");
 
 var fileUtils = require("./lib/file-utils.js");
-var templateEngine = require("./lib/template-engine.js");
 
 var PORT = 8080;
 
 var BASE_DIR = "file_directory";
 
 var app = express();
+
+app.engine(".html", expressHandlebars({extname: ".html"}));
+app.set("view engine", ".html");
+app.set('views', path.join(__dirname, "/public/html/"));
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
@@ -21,8 +25,7 @@ app.get('/*', function(req, res) {
     var directoryName = BASE_DIR + req.url;
     fileUtils.readDirectoryToObject(directoryName).then(
         function(directory) {
-            res.status(200);
-            res.send(templateEngine.getRenderedTemplate(directory));
+            res.render("template", directory);
         }
     ).catch(
         function(err) {
