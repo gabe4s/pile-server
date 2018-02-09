087032cccbfd31aeb50a202d4b5a0d1c18702c3d
Gabe Douda <gabedouda@gmail.com>
2018-02-08 19:16:47 -0700

    file navigation

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+function openFolder(folderName) {
+    var pathName = window.location.pathname;
+    var folderUrl = "";
+
+    folderUrl += pathName;
+    if(pathName[pathName.length-1] != "/") {
+        folderUrl += "/";
+    }
+    folderUrl += folderName;
+    
+    window.location.href = folderUrl;
+}
