e6865920e3f65abf20c9bdac7558c7ed5d797507
Gabe Douda <gabedouda@gmail.com>
2018-05-20 22:08:11 -0600

    file upload working

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var expressHandlebars = require("express-handlebars");
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 var path = require("path");
 
 var fileUtils = require("./lib/file-utils.js");
@@ -15,7 +15,7 @@ var app = express();
 
 app.engine(".html", expressHandlebars({extname: ".html"}));
 app.set("view engine", ".html");
-app.set('views', path.join(__dirname, "/public/html/"));
+app.set("views", path.join(__dirname, "/public/html/"));
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
@@ -23,16 +23,20 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.post('/_fileupload', function(req, res) {
-    handleIncomingFile(req, req.body.directoryName);
-    res.redirect(req.body.directoryName);
+app.post("/*", function(req, res) {
+    var directoryName = BASE_DIR + req.url;
+    fileUtils.handleIncomingFile(req, directoryName).then(
+        function() {
+            res.redirect("back");
+        }
+    )
 });
 
-app.get('/*', function(req, res) {
+app.get("/*", function(req, res) {
     var directoryName = BASE_DIR + req.url;
     fileUtils.readDirectoryToObject(directoryName).then(
         function(directory) {
-            directory.directoryName = directoryName;
+            directory.url = req.url;
             res.render("template", directory);
         }
     ).catch(
